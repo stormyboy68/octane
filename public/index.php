da69a17fac48961ea5c7f9fcee09fb9557c24fb4
abouzar <abouzar.ros1368@gmail.com>
2023-08-27 15:10:01 +0330

    init

--- public/index.php
+++ public/index.php
@@ -1,10 +1,8 @@
 <?php
-
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
